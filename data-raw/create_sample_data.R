7ba73648b33f1dcea7e5dfd1a269cabec8feb9af
Joel R Pradines <joel.pradines@gmail.com>
2025-09-17 21:58:26 -0400

    string parsing

--- data-raw/create_sample_data.R
+++ data-raw/create_sample_data.R
@@ -1,20 +1,96 @@
-network_dt <- data.table::fread("data-raw/raw_network.txt")
-bipartite_dt <- data.table::fread("data-raw/raw_bipartite.txt")
+library(data.table)
 
-network_universe <- unique(c(network_dt[[1]], network_dt[[2]]))
-bipartite_universe <- unique(bipartite_dt$element)
+parse_string_and_go_data <- function() {
 
-common_universe <- intersect(network_universe, bipartite_universe)
+  message("Reading alias file...")
+  alias_file <- "data-raw/9606.protein.aliases.v12.0.txt.gz"
+  header_line <- readLines(alias_file, n = 1)
+  clean_header <- sub("^#", "", header_line)
+  header_names <- strsplit(clean_header, "\\s+")[[1]]
+  alias_dt <- data.table::fread(alias_file, skip = 1, header = FALSE)
+  data.table::setnames(alias_dt, old = names(alias_dt), new = header_names)
 
-network_subset_dt <- network_dt[element1 %in% common_universe & element2 %in% common_universe]
-bipartite_subset_dt <- bipartite_dt[element %in% common_universe]
+  message("Preparing mapping table...")
+  entrez_source_name <- "Ensembl_HGNC_entrez_id" # Confirmed source name
+  mapping_dt <- alias_dt[source == entrez_source_name, .(
+    protein_id = string_protein_id,
+    gene_id = alias
+  )]
+  mapping_dt[, protein_id := sub("9606.", "", protein_id, fixed = TRUE)]
+  mapping_dt <- unique(mapping_dt)
+  data.table::setkey(mapping_dt, protein_id)
+
+  message("Reading and processing protein network file...")
+  network_dt <- data.table::fread("data-raw/9606.protein.links.v12.0.txt")
+
+  network_dt[, protein1 := sub("9606.", "", protein1, fixed = TRUE)]
+  network_dt[, protein2 := sub("9606.", "", protein2, fixed = TRUE)]
+
+  network_dt[, c("p_canon_1", "p_canon_2") := .(pmin(protein1, protein2), pmax(protein1, protein2))]
+
+  protein_network_dt <- network_dt[, .(best_score = max(combined_score)), by = .(p_canon_1, p_canon_2)]
 
 
-ecg <- ECGraph(network_subset_dt)
-lst <- trim_ecgraph(ecg, 0.5)
-ecg <- remove_isolated_vertices(lst[["trimmed_graph"]])
+  message("Mapping protein IDs to gene IDs...")
+  protein_network_dt[mapping_dt, on = .(p_canon_1 = protein_id), gene1 := i.gene_id]
+  protein_network_dt[mapping_dt, on = .(p_canon_2 = protein_id), gene2 := i.gene_id]
 
-ects <- ECTermScoring(bipartite_subset_dt)
-lst <- trim_bipartite_terms(ects, 0.2)
-ects <- remove_isolated_elements(lst[["trimmed_object"]])
-ects <- remove_empty_terms(ects)
+
+  message("Cleaning final gene network...")
+
+  gene_network_with_scores <- na.omit(protein_network_dt, cols = c("gene1", "gene2"))
+
+  gene_network_with_scores <- gene_network_with_scores[gene1 != gene2]
+
+  gene_network_with_scores[, c("g_canon_1", "g_canon_2") := .(pmin(gene1, gene2), pmax(gene1, gene2))]
+
+  final_gene_network <- gene_network_with_scores[,
+                                                 .(best_combined_score = max(best_score)),
+                                                 by = .(g_canon_1, g_canon_2)
+  ]
+
+  # Clean up the column names for the final output
+  data.table::setnames(final_gene_network, c("g_canon_1", "g_canon_2"), c("gene1", "gene2"))
+
+  message("Done.")
+  return(final_gene_network)
+}
+
+
+calculate_network_properties_by_threshold <- function(dt) {
+
+  if (!is.data.table(dt) || nrow(dt) == 0) {
+    return(data.table(threshold = numeric(), order = integer(), size = integer()))
+  }
+
+ summary_dt <- dt[, .(
+    size_of_block = .N,
+    nodes_in_block = list(unique(c(gene1, gene2)))
+  ), by = best_combined_score]
+
+  data.table::setorder(summary_dt, -best_combined_score)
+
+  summary_dt[, cumulative_size := cumsum(size_of_block)]
+
+  cumulative_node_sets <- Reduce(union, summary_dt$nodes_in_block, accumulate = TRUE)
+
+  summary_dt[, cumulative_order := lengths(cumulative_node_sets)]
+
+  final_summary <- summary_dt[, .(
+    threshold = best_combined_score,
+    order = cumulative_order,
+    size = cumulative_size
+  )]
+
+  data.table::setorder(final_summary, threshold)
+
+  return(final_summary)
+}
+
+# --- EXAMPLE ---
+dt <- parse_string_and_go_data()
+data.table::setorder(dt, best_combined_score)
+network_summary <- calculate_network_properties_by_threshold(dt)
+print(head(network_summary))
+print(tail(network_summary))
+data.table::fwrite(network_summary, "data-raw/network_summary.txt", sep = "\t")
