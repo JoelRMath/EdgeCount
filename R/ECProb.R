ec5d5a09226c87bfa2e81ebb53a63e1371ee51de
Joel R Pradines <joel.pradines@gmail.com>
2025-09-27 09:26:58 -0400

    finalized merge testing

--- R/ECProb.R
+++ R/ECProb.R
@@ -531,7 +531,7 @@ setMethod("summarize_suitability_fast",
             ))
           })
 
-#' @title Get Disjoint Sets for Multiple Term Pairs (Vectorized version)
+#' @title Get Disjoint Sets for Multiple Term Pairs (Vectorized version more)
 #'
 #' @description A high-performance, vectorized function that calculates the
 #' disjoint element sets for a list of term pairs.
